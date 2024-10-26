77e6f3b34e1829815262e322c79d0b366159b20a
alanbatrez <alanbatrez@hotmail.com>
2024-10-26 00:07:28 -0700

    Update StarWarsName.java
    created a new method to take the first character of the String and return it Upper case

--- src/code/StarWarsName.java
+++ src/code/StarWarsName.java
@@ -1,7 +1,7 @@
 /**
- * This class formats a name as a Star Wars name
- * @author Alan Riveros
- * @author Pedro Perez
+ * This class formats a name as a Star Wars name.
+ * @author Alan Riveros.
+ * @author Pedro Perez.
  * @version 1.0
  */
 public class StarWarsName
@@ -14,14 +14,16 @@ public class StarWarsName
     {
         final int[] FIRST_TWO_LETTERS;
         final int[] FIRST_THREE_LETTERS;
+        final int EXPECTED_ARGUMENTS;
 
         FIRST_TWO_LETTERS = new int[]{0,2};
         FIRST_THREE_LETTERS = new int[]{0,3};
+        EXPECTED_ARGUMENTS = 4;
 
         final String[] allWords;
         allWords = args[0].split("\\|");
 
-        if(allWords.length != 4)
+        if(allWords.length != EXPECTED_ARGUMENTS)
         {
             throw new IllegalArgumentException("ERROR: Incorrect number of words provided.");
         }
@@ -39,9 +41,23 @@ public class StarWarsName
         final String starWarsFirstName;
         final String starWarsLastName;
 
+
         starWarsFirstName = firstName.substring(FIRST_THREE_LETTERS[0],FIRST_THREE_LETTERS[1] ) + lastName.substring(FIRST_TWO_LETTERS[0], FIRST_TWO_LETTERS[1]).toLowerCase();
         starWarsLastName = motherMaidenName.substring(FIRST_TWO_LETTERS[0], FIRST_TWO_LETTERS[1]) + city.substring(FIRST_THREE_LETTERS[0], FIRST_THREE_LETTERS[1]).toLowerCase();
 
-        
+        System.out.format("Your Star Wars Name is: %s %s%n", UpperCaseFirstLetter(starWarsFirstName) , UpperCaseFirstLetter(starWarsLastName));
+
+    }
+
+    /**
+     * Method that return the first character of the word in Upper Case(e.g. pedro -> Pedro).
+     * @param stringToEvaluate Takes the word to extract the first character.
+     * @return Return the final concatenated String.
+     */
+    private static String UpperCaseFirstLetter(final String stringToEvaluate)
+    {
+        final char firstLetter = Character.toUpperCase(stringToEvaluate.charAt(0));
+
+        return firstLetter + stringToEvaluate.substring(1).toLowerCase();
     }
 }
